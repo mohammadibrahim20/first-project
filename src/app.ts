6a70f0329e2efe738f24786674a17eb3453b0f3c
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-11-23 14:49:18 +0600

    Student Create Done

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,18 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/config/modules/student/student.route';
 const app: Application = express();
-const port = 3000;
 
 // parser
 app.use(express.json());
 app.use(cors());
 
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
 
 export default app;
---
+++ src/app/config/modules/student/student.service.ts
@@ -0,0 +1,23 @@
+import { StudentModel } from './student.model';
+import { Student } from './studnt.interface';
+
+const createStudentIntoDB = async (student: Student) => {
+  const result = await StudentModel.create(student);
+  return result;
+};
+
+const getAllStudentsFromDB = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
+
+const getSingleStudentFromDB = async (id: string) => {
+  const result = await StudentModel.findOne({ id });
+  return result;
+};
+
+export const StudentServices = {
+  createStudentIntoDB,
+  getAllStudentsFromDB,
+  getSingleStudentFromDB,
+};
